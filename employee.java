dda90d5bbd8a7a0c01674e5c821829e95a270360
himanshugupta005 <80818447+himanshugupta005@users.noreply.github.com>
2021-09-10 17:14:38 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,18 @@
+public class employee {
+    String name,address;
+    int year;
+public static void main(String args[])          
+{
+    System.out.println("Name  Year of joining  Address");
+    System.out.println("Aman     2021          Chandigarh");
+    System.out.println("Sunil    2000          Mathura");
+    System.out.println("Rohan    2010          Agra");
+
+}
+}
+
+
+
+
+  
+
