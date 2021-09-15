00c52c33f8730a6f2f91d5b18a406a4e470c52e2
himanshugupta005 <80818447+himanshugupta005@users.noreply.github.com>
2021-09-15 19:47:33 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,18 @@
+public class calculator {
+  static double powerInt(int num1, int num2){
+      return Math.pow(num1,num2);
+  }
+  static double powerDouble(double num1,int num2){
+      return Math.pow(num1,num2);
+  }
+  class calculator1{
+      public static void main(String[]args){
+          System.out.println(calculator.powerInt(85,3));
+          System.out.println(calculator.powerDouble(85.0,2));
+      }
+    }
+}
+
+      
+    
+
